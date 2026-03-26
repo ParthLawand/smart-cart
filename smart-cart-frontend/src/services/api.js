2d3a65885d9f6392c10346def746f960e48f1dc9
ParthLawand <parthlawand10@gmail.com>
2026-03-27 00:34:12 +0530

    chore: deployment prep - add start script, env variable for API URL

--- smart-cart-frontend/src/services/api.js
+++ smart-cart-frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: API_BASE
 });
 
 // Attach JWT token to every request
